dc4ca6cb220799157e516d2eb810ce87945cf564
Ahmed Essam <aesam@devsolutions.team>
2024-05-30 12:37:17 +0300

    adjust details screen

--- app/details.tsx
+++ app/details.tsx
@@ -1,32 +1,48 @@
 import React from 'react';
-import {Image, View, Text,TouchableOpacity,FlatList } from 'react-native';
-import { useLocalSearchParams } from 'expo-router';
+import {Image, View, Text,TouchableOpacity,FlatList,ScrollView } from 'react-native';
+import { useLocalSearchParams,router } from 'expo-router';
 import { useArticles } from '../apis/NewsApi';
 import NewsCard from '../components/NewsCard'
 const Details = () => {
   const params = useLocalSearchParams();
   const { data } = useArticles();
-  const articlesToShow = data.articles.slice(1, 3);
+  const articlesToShow = data.articles.slice(3, 5);
   return (
-    <View className='my-4 py-4 px-2 mx-2 h-full'>
+    <ScrollView className='my-4 py-4 px-2 mx-2 h-full'>
    
          <Text className='font-bold text-lg '>{params.title}</Text>
          <Text className='text-sm'> By {params.author}</Text>
          <Image 
+          resizeMode={'contain'}
           className='w-full h-40 mt-2 '
            source={params.urlToImage ? { uri: params.urlToImage } :  require('../assets/images/empty.png')}
          />
          <Text className=' my-2'>{params.description}</Text>
-       <View className='border my-8'></View>
+       <View className='border-[0.5px] my-8'></View>
          <Text className='font-bold text-lg  mb-2'>Similar News</Text>
          <FlatList data={articlesToShow}
+         horizontal
                 keyExtractor={(item, index) => 'key' + index}
                 renderItem={({ item }) => {
-                    return   <NewsCard item={item} />
+                    return  (
+                      <TouchableOpacity onPress={()=>router.push({ pathname: `/details`, params: item })} className='my-4 py-2 px-4 mx-4 block rounded-lg bg-white w-[300px] h-[300px]'>
+                      <Text className='font-bold text-lg w-full '>{item.title.substring(
+                            0,
+                            45
+                          ) + "..."}</Text>
+                      <Text className='text-sm'> By {item.author}</Text>
+                      <Image 
+                      resizeMode={'contain'}
+                       className='w-full h-40 mt-2 '
+                        source={item.urlToImage ? { uri: item.urlToImage } :  require('../assets/images/empty.png')}
+                      />
+ 
+                  </TouchableOpacity>
+                    )
                     
                 }}
             />
-  </View>
+  </ScrollView>
   );
 }
 export default Details;
